a8983be7bafa6e3f3e4e35fa0742d2b771281de0
Phillip Schichtel <phillip.schichtel@vier.ai>
2026-01-23 13:50:41 +0100

    include the webcomponent bundle when publishing

--- publish.sh
+++ publish.sh
@@ -2,9 +2,12 @@
 
 set -eu
 
-rm -Rv dist
+npm install
+rm -Rv dist || echo "Dist already gone"
 npx tsc
+npx webpack bundle --env mode=production
 cp -v package.json dist
 cd dist
-rm -v ./*-example.* webcomponent.*
+rm -v ./*example* webcomponent.*
+
 npm publish --access=public "$@"
